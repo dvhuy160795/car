382ccb6dfff682cb6e9bf243dbbe0ec723fb1997
huydv <huydv@vnext.com.vn>
2018-08-17 19:50:00 +0700

    11

--- views/client/Home/Home.php
+++ views/client/Home/Home.php
@@ -11,16 +11,36 @@
                 <div class="row carousel-holder">
 
                     <div class="col-md-12">
-                        <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-  
-                            <div class="carousel-inner">
-                                <div class="item active" style="background-image: url(./style/images/11.jpg); height:400px"> </div><!--/.item-->
-                                <div class="item" style="background-image: url(./style/images/22.jpg); height:400px"></div><!--/.item-->
-                                <div class="item" style="background-image: url(./style/images/33.jpg); height:400px"></div><!--/.item-->
-                            </div>
-                            </div>
-                        </div>
+                        <div id="demo" class="carousel slide" data-ride="carousel">
 
+                          <!-- Indicators -->
+                          <ul class="carousel-indicators">
+                            <li data-target="#demo" data-slide-to="0" class="active"></li>
+                            <li data-target="#demo" data-slide-to="1"></li>
+                            <li data-target="#demo" data-slide-to="2"></li>
+                          </ul>
+
+                          <!-- The slideshow -->
+                          <div class="carousel-inner">
+                            <div class="carousel-item active">
+                              <img src="./style/images/11.jpg" alt="Los Angeles" width="1100" height="500">
+                            </div>
+                            <div class="carousel-item">
+                              <img src="./style/images/22.jpg" alt="Chicago" width="1100" height="500">
+                            </div>
+                            <div class="carousel-item">
+                              <img src="./style/images/33.jpg" alt="New York" width="1100" height="500">
+                            </div>
+                          </div>
+
+                          <!-- Left and right controls -->
+                          <a class="carousel-control-prev" href="#demo" data-slide="prev">
+                            <span class="carousel-control-prev-icon"></span>
+                          </a>
+                          <a class="carousel-control-next" href="#demo" data-slide="next">
+                            <span class="carousel-control-next-icon"></span>
+                          </a>
+                        </div>
                     </div>
  	
                 </div>
--- views/client/layout.php
+++ views/client/layout.php
@@ -13,7 +13,10 @@
     <link href="./style/upload/12.jpg" rel="shortcut icon" type="image/x-icon" />
     <title><?php echo $control."/".$action?></title>
 
-
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
     <!-- Bootstrap Core CSS -->
     <link href="./style/css/bootstrap.min.css" rel="stylesheet">
 
@@ -57,7 +60,6 @@
    ?>
     </div>
 </body>
-<script src="./style/js/bootstrap.min.js"></script>
 </html>
 
 
